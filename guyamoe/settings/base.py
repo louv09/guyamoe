64c88f9cc20f6a01c1b61c7b41b64f5584d0d30d
Love Goyal <89312375+louv09@users.noreply.github.com>
2025-06-05 23:14:56 +0530

    Update base.py

--- guyamoe/settings/base.py
+++ guyamoe/settings/base.py
@@ -27,7 +27,7 @@ CANONICAL_ROOT_DOMAIN = "127.0.0.1:8000"
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://roml-moe.onrender.com']
 
 SITE_ID = 1
 
